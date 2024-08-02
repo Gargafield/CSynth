db811d66bcf4267b6e03691c7d4bdcd2ed2941eb
Gargafield <gargafield@outlook.com>
2024-08-02 15:02:55 +0200

    Remove RVSDG implementation

--- Analysis/RVSDG/Id.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CSynth.Analysis.RVSDG;
-
-public enum NodeId : uint {}
-public enum EdgeId : uint {}
-public enum LinkId : uint {}
-public record PortId(NodeId NodeId, uint PortIndex) { }
-
