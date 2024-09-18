83b04eab5ec4636753ac8a07806ade394801219b
Gargafield <gargafield@outlook.com>
2024-09-18 14:49:26 +0200

    Add Fibonacci example

---
+++ examples/fibonacci/Fibonacci.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Fibonacci
+{
+    static void Main()
+    {
+        int n = 50;
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        for (int i = 0; i < n; i++) {
+            Console.WriteLine(fib[i]);
+        }
+    }
+}
---
+++ examples/fibonacci/fibonacci.lua
@@ -0,0 +1,36 @@
+local Object = require("@System/Object")
+local Console = require("@System/Console")
+local Module = {}
+local Fibonacci = {}
+function Fibonacci.Main()
+    local local_0, local_1, local_2, local_3
+    local_0 = 50
+    local_1 = table.create(local_0)
+    local_1[0] = 0
+    local_1[1] = 1
+    local_2 = 2
+    repeat
+        if local_2 < local_0 then
+            local_1[local_2] = local_1[local_2 - 1] + local_1[local_2 - 2]
+            local_2 = local_2 + 1
+            LoopControl = 1
+        else
+            LoopControl = 0
+        end
+    until LoopControl == 0
+    local_3 = 0
+    repeat
+        if local_3 < local_0 then
+            Console.WriteLine(local_1[local_3])
+            local_3 = local_3 + 1
+            LoopControl = 1
+        else
+            LoopControl = 0
+        end
+    until LoopControl == 0
+end
+function Fibonacci.new(this)
+    Object.new(this)
+end
+Fibonacci.Main()
+
