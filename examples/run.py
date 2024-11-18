3d303bd95d0922a33195518019aa4802f115f120
Gargafield <gargafield@outlook.com>
2024-11-18 14:49:31 +0100

    Add the ability to run all examples with one command

--- examples/run.py
+++ examples/run.py
@@ -6,55 +6,54 @@ import shutil
 script_path = os.path.abspath(__file__).replace("run.py", "")
 examples = [example for example in os.listdir(script_path) if os.path.isdir(os.path.join(script_path, example))]
 
-# Get input project name from user from args
-example_name = str(sys.argv[1]).strip().lower()
-
-# Check if project exists
-if example_name not in examples:
-    print(f"Example {example_name} not found")
-    print(f"Available examples: {examples}")
-    sys.exit(1)
-
 # Get path to repo root
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
 cli_path = f"{project_root}/src/CLI/CSynth.CLI.csproj"
-example_path = f"{project_root}/examples/{example_name}"
-example_dll_path = f"{example_path}/bin/Release/net8.0/{example_name}.dll"
-output_path = f"{example_path}/{example_name}.lua"
 
-# Build example and wait for it to finish
-os.system(f"dotnet build -c Release {example_path}")
+def compile_example(example_name):
+    example_path = f"{project_root}/examples/{example_name}"
+    example_dll_path = f"{example_path}/bin/Release/net8.0/{example_name}.dll"
+    output_path = f"{example_path}/{example_name}.lua"
+    
+    # Build example and wait for it to finish
+    os.system(f"dotnet build -c Release {example_path}")
 
-# Run example and capture stdout
-result = subprocess.run(["dotnet", "run", "--project", cli_path, "--", example_dll_path], capture_output=True, text=True)
+    # Compile example and capture stdout
+    result = subprocess.run(["dotnet", "run", "--project", cli_path, "--", example_dll_path], capture_output=True, text=True)
+    
+    if result.returncode != 0:
+        print(result.stderr)
+        sys.exit(1)
+    
+    # Write to example output
+    with open(output_path, 'w') as f:
+        f.write(result.stdout)
 
-if result.returncode != 0:
-    print(result.stderr)
-    sys.exit(1)
+    print(f"Output written to {output_path}")    
 
-# Write to example output
-with open(output_path, 'w') as f:
-    f.write(result.stdout)
+def run_example(example_name):
+    example_path = f"{project_root}/examples/{example_name}"
+    output_path = f"{example_path}/{example_name}.lua"
 
-print(f"Output written to {output_path}")    
+    # if there's a output.txt file, try compare lua output with it
+    output_txt_path = f"{example_path}/output.txt"
+    input_txt_path = f"{example_path}/input.txt"
 
-# if there's a output.txt file, try compare lua output with it
-output_txt_path = f"{example_path}/output.txt"
-input_txt_path = f"{example_path}/input.txt"
-
-if os.path.exists(output_txt_path):
-    with open(output_txt_path, 'r') as f:
-        expected_output = f.read()
-
-    # run example.lua and capture stdout
-    lune_executable = shutil.which("lune")
-
+    expected_output = ""
     input_contents = ""
+
+    if not os.path.exists(output_txt_path):
+        print(f"output.txt file not found for {example_name}, skipping comparison")
+    else:
+        with open(output_txt_path, 'r') as f:
+            expected_output = f.read()
+
     if os.path.exists(input_txt_path):
         with open(input_txt_path, 'r') as f:
             input_contents = f.read()
 
+    # run example.lua and capture stdout
+    lune_executable = shutil.which("lune")
 
     if lune_executable:
         args = [lune_executable, "run", output_path]
@@ -76,6 +75,21 @@ if os.path.exists(output_txt_path):
         print(expected_output)
         print("Actual:")
         print(result.stdout)
-else:
-    print(f"No output.txt file found for {example_name},  won't compare output")
 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: run.py [example_name/all]")
+        sys.exit(1)
+
+    example_name = sys.argv[1]
+
+    if example_name in examples:
+        compile_example(example_name)
+        run_example(example_name)
+    elif example_name == "all":
+        for example in examples:
+            compile_example(example)
+            run_example(example)
+    else:
+        print(f"Example {example_name} not found")
+        sys.exit(1)
